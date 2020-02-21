89cbb14c3cba51edc1442efb0973d71f014c6e45
soni653 <dave.family653@outlook.com>
2020-02-20 22:08:59 -0500

    Done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -24,7 +24,7 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -53,3 +53,75 @@ const followersArray = [];
   luishrd
   bigknell
 */
+
+const followersArray = ['bschatzj','jambis','berdiyeva','tetondan','dustinmyers','justsml','luishrd','bigknell'];
+followersArray.forEach(item => {axios.get(`https://api.github.com/users/${item}`).then(response => {
+  cards.append(GithubCard(response.data))
+  });
+})
+
+const cards = document.querySelector('.cards');
+axios.get('https://api.github.com/users/soni653')
+.then((response) =>{ 
+  cards.append(GithubCard(response.data))
+  console.log(response);
+})
+
+.catch((error => {
+  console.log('Info not found', error)
+}))
+
+
+
+
+
+function GithubCard(obj){
+  const hubcard = document.createElement('div');
+  const hubimg = document.createElement('img');
+  const hubInfo = document.createElement('div');
+  const hubName = document.createElement('h3');
+  const hubUser = document.createElement('p');
+  const hubLocation = document.createElement('p');
+  const hubProfile = document.createElement('p');
+  const hubUrl = document.createElement('a');
+  const hubFollowers = document.createElement('p');
+  const hubFollowing = document.createElement('p');
+  const hubBio = document.createElement('p');
+
+
+  hubcard.append(hubimg,hubInfo);
+  hubInfo.append(hubName,hubUser,hubLocation,hubProfile,hubFollowers,hubFollowing,hubBio);
+  hubProfile.append(hubUrl);
+
+  hubcard.classList.add('card');
+  hubInfo.classList.add('card-info');
+  hubName.classList.add('name');
+  hubUser.classList.add('username');
+
+  hubimg.src = obj.avatar_url;
+  hubName.textContent = obj.name;
+  hubUser.textContent = obj.login;
+  hubLocation.textContent = obj.location;
+  hubUrl.textContent = obj.html_url;
+  hubFollowers.textContent = obj.followers;
+  hubFollowing.textContent = obj.following;
+  hubBio.textContent = obj.hubBio
+
+  return hubcard;
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
